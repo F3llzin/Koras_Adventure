b6bfd89b2212033a289ca410dcb98448fbde072b
F3llzin <fellipefernandes784@gmail.com>
2025-11-20 18:22:50 -0300

    Mudanças pt2.
    Alteração na fase de instuções e add de sprites animados.

--- Scripts/espinhos_area.gd
+++ Scripts/espinhos_area.gd
@@ -2,10 +2,9 @@ extends Area2D
 @onready var collision = $collision
 @onready var espinhos = $espinhos
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	collision.shape.size = espinhos.get_rect().size
 
 func _on_body_entered(body):
 	if body.name == "jogador" && body.has_method("tomar_dano"):
-		body.tomar_dano(Vector2(0, -250))
+		body.tomar_dano(Vector2((global_position.x - body.global_position.x) * -20, -375))
--- scripts/Telainicio.gd
+++ scripts/Telainicio.gd
@@ -13,7 +13,7 @@ func _process(_delta):
 
 func _on_button_start_pressed():
 	Global.fase = 0
-	get_tree().change_scene_to_file("res://instruct.tscn")
+	get_tree().change_scene_to_file("res://seletor_fases.tscn")
 
 func _on_button_quit_pressed():
 	get_tree().quit()
